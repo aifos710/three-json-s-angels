c79385e44f73f73a1bfe510308e7cf4333643c3d
AmySeo12 <maryori.amy@gmail.com>
2016-11-14 17:35:31 -0500

    agregando imagenes al servidor

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,27 @@
-$(document).ready(function(){
-
-	//lenguajes
-	$(document).ready(function() {
-    $('select').material_select();
-  });
-
-	// 
+$(document).ready(function() {
+	$('select').material_select();
 	$('ul.tabs').tabs();
-   
-   /* $('.dropdown-button').dropdown('open');
-    $('.dropdown-button').dropdown('close');*/
-	
-})
 
+	var socket= io();
+	socket.on("addimage",function(msg, base64image){
+		console.log(base64image);
+		$(".mensajes").append(
+			$("<p>").append(
+				$("<b>").text(msg), "<a target='_blank' href='"+base64image+"'><img src='"+base64image+"'/></a>"
+			)
+		);
+	});
+
+	$(function(){
+		//cuando la página cargue
+		$("#imageFile").on("change", function(e){
+			var file= e.originalEvent.target.files[0];
+			var reader = new FileReader();
+			reader.onload = function(evt){
+				//se envia la imagen resultante
+				socket.emit("userimage", evt.target.result);
+			};
+			reader.readAsDataURL(file);
+		});
+	})
+});
--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 var express= require("express");
 var app= express();
+var http= require("http").Server(app);
+var io= require("socket.io")(http);
 
 app.use(express.static(__dirname + "/public"));
-app.listen(1234, function(){
+
+app.set("public", __dirname+ "/public");
+
+//sockets
+
+io.sockets.on("connection", function(socket){
+	socket.on("userimage", function(imagen){
+		io.sockets.emit("addimage", "Image Received", imagen);
+	})
+})
+app.get("/", function(req,res){
+	res.render("search.html");
+
+})
+
+http.listen(3000, function(){
 	console.log("Servidor encendido.");
 });
