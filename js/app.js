1485b2b636524d9c71b5657f9e0e241902a72c82
aifos710 <sofia.m.caballero@gmail.com>
2016-11-14 12:54:16 -0500

    interfaz

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+	//lenguajes
+	$(document).ready(function() {
+    $('select').material_select();
+  });
+
+	// 
+	$('ul.tabs').tabs();
+   
+   /* $('.dropdown-button').dropdown('open');
+    $('.dropdown-button').dropdown('close');*/
+	
+})
+
